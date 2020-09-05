5a39b12de10e3b45a1431a04716a81784c774dd1
Danilo Castro <daniloster@gmail.com>
2020-09-05 23:07:35 +0100

    removed pause button

--- src/GamePlay/states/Toolbar.js
+++ src/GamePlay/states/Toolbar.js
@@ -8,39 +8,21 @@ className.scope(
   `
 padding: 0.5rem 0;
 display: flex;
-justify-content: space-between;
+justify-content: center;
 `,
 )
 className.scope('button', 'padding: 0.5rem;')
 className.scope('*:last-child', 'display: none;')
 className.modifier('.gameOver *:last-child', 'display: block;')
 className.modifier('.gameOver *:not(:last-child)', 'display: none;')
-className.modifier('.initial *:first-child', 'display: none;')
-className.modifier('.initial', 'justify-content: center;')
 
 export default function Toolbar({ state }) {
   return new Component('div', {
     className,
     classList: ObservableState.observeTransform(state, (value) => ({
       gameOver: value.includes(GameState.GameOver),
-      initial: value.includes(GameState.Initial),
     })),
     children: [
-      new Component('button', {
-        attrs: { type: 'button' },
-        children: ObservableState.observeTransform(state, (values) =>
-          !values.includes(GameState.Running) ? 'Resume' : 'Pause',
-        ),
-        events: {
-          click: (e) => {
-            state.set((value) =>
-              value !== GameState.Running
-                ? GameState.Running
-                : GameState.Paused,
-            )
-          },
-        },
-      }),
       new Component('button', {
         attrs: { type: 'button' },
         children: 'Exit',
