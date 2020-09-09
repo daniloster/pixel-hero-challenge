9dc8e3a850a172c44144391beb471f08f3d176dc
Danilo Castro <daniloster@gmail.com>
2020-09-09 01:03:17 +0100

    added message component

---
+++ src/common/components/Message.js
@@ -0,0 +1,22 @@
+import ObservableState from '../ObservableState'
+import Component from '../ui/Component'
+import CSS from '../ui/CSS'
+
+const className = new CSS('message')
+className.scope('')
+className.modifier('.is-error', 'color: red;')
+
+export default function Message({ message, isError = false }) {
+  return new Component('p', {
+    className,
+    children: message,
+    classList: {
+      'is-error': isError,
+    },
+    style: {
+      display: ObservableState.observeTransform(message, (val) =>
+        !!val ? 'block' : 'none',
+      ),
+    },
+  })
+}
