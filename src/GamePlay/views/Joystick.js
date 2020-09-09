08f12b03a36de2f21a3fd83f0dac2039ec70dd7f
Danilo Castro <daniloster@gmail.com>
2020-09-09 20:09:11 +0100

    prevent double tap zoom

--- src/GamePlay/views/Joystick.js
+++ src/GamePlay/views/Joystick.js
@@ -60,13 +60,51 @@ className.scope(
   `,
 )
 
+// Ensure touches occur rapidly
+const delay = 500
+// Sequential touches must be in close vicinity
+const minZoomTouchDelta = 10
+
+// Track state of the last touch
+let lastTapAt = 0
+let lastClientX = 0
+let lastClientY = 0
+function preventDoubleTapZoom(event) {
+  // Exit early if this involves more than one finger (e.g. pinch to zoom)
+  if (event.touches.length > 1) {
+    return
+  }
+
+  const tapAt = new Date().getTime()
+  const timeDiff = tapAt - lastTapAt
+  const { clientX, clientY } = event.touches[0]
+  const xDiff = Math.abs(lastClientX - clientX)
+  const yDiff = Math.abs(lastClientY - clientY)
+  if (
+    xDiff < minZoomTouchDelta &&
+    yDiff < minZoomTouchDelta &&
+    event.touches.length === 1 &&
+    timeDiff < delay
+  ) {
+    event.preventDefault()
+    // Trigger a fake click for the tap we just prevented
+    event.target.click()
+  }
+  lastClientX = clientX
+  lastClientY = clientY
+  lastTapAt = tapAt
+}
+
 export default function Joystick({ joystick, state }) {
   const button = (direction) =>
     new Component('button', {
       attrs: {
         type: 'button',
       },
-      events: { click: joystick[direction] },
+      events: {
+        click: joystick[direction],
+        touchstart: preventDoubleTapZoom,
+      },
       className: `joysitck-pad-direction-${direction}`,
       children: `<i class="fa fa-arrow-${direction}" aria-hidden="true"></i>`,
       html: true,
