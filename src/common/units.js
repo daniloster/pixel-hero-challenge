f8d40ef8004c4f0f137a8c91efe616dee2bc18ca
Danilo Castro <daniloster@gmail.com>
2020-09-12 16:40:08 +0100

    changed max rows and columns

--- src/common/units.js
+++ src/common/units.js
@@ -1,5 +1,5 @@
 export const UNIT_SIZE = 40
-export const MAX_COLUMNS = 30 // (total of 40 * 25 pixels = 1000)
-export const MAX_ROWS = 20 // (total of 40 * 15 pixels = 600)
+export const MAX_COLUMNS = 15
+export const MAX_ROWS = 15
 export const MIN_COLUMNS = 3
 export const MIN_ROWS = 3
